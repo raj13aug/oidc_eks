3e29138bae705c989667ab86d1b10e472c2d85cf
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-05 17:21:58 +0530

    Create rbac.tf

---
+++ eks_oidc/rbac.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_cluster_role_binding" "admin" {
+  metadata {
+    name = "admin"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "aad:248412de-c401-4202-b978-d4761b355107"
+    name      = "admin"
+  }
+}
